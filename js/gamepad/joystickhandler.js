a9e469ebf0546fafa4f7cafbd359ea93a85b7773
Eric Barch <ericb@ericbarch.com>
2013-01-19 20:58:14 -0500

    Joystick controller creation.

--- js/gamepad/joystickhandler.js
+++ js/gamepad/joystickhandler.js
@@ -19,6 +19,17 @@ define([
       joy3_mapped: false,
       joy4_mapped: false,
 
+      joy1_controller: null,
+      joy2_controller: null,
+      joy3_controller: null,
+      joy4_controller: null,
+
+      // store the unique identifier of the joystick
+      joy1_id: null,
+      joy2_id: null,
+      joy3_id: null,
+      joy4_id: null,
+
       // keep track of components that change (lookup)
       componentCache: {},
 
@@ -39,47 +50,73 @@ define([
         this.callbacks.push(callback);
       },
 
+      initializeController: function(joyController) {
+        // default axis controls
+        joyController.create({alias: 'Left Stick Horizontal', axis: true, gamepadIndex: 0, bundleIndex: 0});
+        joyController.create({alias: 'Left Stick Vertical', axis: true, gamepadIndex: 1, bundleIndex: 1});
+        joyController.create({alias: 'Right Stick Horizontal', axis: true, gamepadIndex: 2, bundleIndex: 2});
+        joyController.create({alias: 'Right Stick Vertical', axis: true, gamepadIndex: 3, bundleIndex: 3});
+
+        // default button controls
+        joyController.create({alias: 'A Button', axis: false, gamepadIndex: 0, bundleIndex: 4});
+        joyController.create({alias: 'B Button', axis: false, gamepadIndex: 1, bundleIndex: 5});
+        joyController.create({alias: 'X Button', axis: false, gamepadIndex: 2, bundleIndex: 6});
+        joyController.create({alias: 'Y Button', axis: false, gamepadIndex: 3, bundleIndex: 7});
+        joyController.create({alias: 'Left Shoulder', axis: false, gamepadIndex: 4, bundleIndex: 8});
+        joyController.create({alias: 'Right Shoulder', axis: false, gamepadIndex: 5, bundleIndex: 9});
+        joyController.create({alias: 'Left Trigger', axis: false, gamepadIndex: 6, bundleIndex: 10});
+        joyController.create({alias: 'Right Trigger', axis: false, gamepadIndex: 7, bundleIndex: 11});
+        joyController.create({alias: 'Select', axis: false, gamepadIndex: 8, bundleIndex: 12});
+        joyController.create({alias: 'Start', axis: false, gamepadIndex: 9, bundleIndex: 13});
+        joyController.create({alias: 'Left Stick Button', axis: false, gamepadIndex: 10, bundleIndex: 14});
+        joyController.create({alias: 'Right Stick Button', axis: false, gamepadIndex: 11, bundleIndex: 15});
+        joyController.create({alias: 'D-Pad Up', axis: false, gamepadIndex: 12, bundleIndex: 16});
+        joyController.create({alias: 'D-Pad Down', axis: false, gamepadIndex: 13, bundleIndex: 17});
+        joyController.create({alias: 'D-Pad Left', axis: false, gamepadIndex: 14, bundleIndex: 18});
+        joyController.create({alias: 'D-Pad Right', axis: false, gamepadIndex: 15, bundleIndex: 19});
+        joyController.create({alias: 'Aux One', axis: false, gamepadIndex: 16, bundleIndex: 20});
+        joyController.create({alias: 'Aux Two', axis: false, gamepadIndex: 17, bundleIndex: 21});
+        joyController.create({alias: 'Aux Three', axis: false, gamepadIndex: 18, bundleIndex: 22});
+        joyController.create({alias: 'Aux Four', axis: false, gamepadIndex: 19, bundleIndex: 23});
+      },
+
       /**
        * Update the gamepads on the screen, creating new elements from the
        * template.
        */
       updateGamepads: function(gamepads) {
 
-        /*
-
-          var joy1 = new ControllerCollection();
-
-          joy1.create({alias: 'Left Stick Horizontal', axis: true, gamepad-index: 0, bundle-index: 0});
-          joy1.create({alias: 'Left Stick Vertical', axis: true, gamepad-index: 1, bundle-index: 1});
-          joy1.create({alias: 'Right Stick Horizontal', axis: true, gamepad-index: 2, bundle-index: 2});
-          joy1.create({alias: 'Right Stick Vertical', axis: true, gamepad-index: 3, bundle-index: 3});
-
-          joy1.create({alias: 'A Button', axis: false, gamepad-index: 0, bundle-index: 4});
-          joy1.create({alias: 'B Button', axis: false, gamepad-index: 1, bundle-index: 5});
-          joy1.create({alias: 'X Button', axis: false, gamepad-index: 2, bundle-index: 6});
-          joy1.create({alias: 'Y Button', axis: false, gamepad-index: 3, bundle-index: 7});
-          joy1.create({alias: 'Left Shoulder', axis: false, gamepad-index: 4, bundle-index: 8});
-          joy1.create({alias: 'Right Shoulder', axis: false, gamepad-index: 5, bundle-index: 9});
-          joy1.create({alias: 'Left Trigger', axis: false, gamepad-index: 6, bundle-index: 10});
-          joy1.create({alias: 'Right Trigger', axis: false, gamepad-index: 7, bundle-index: 11});
-          joy1.create({alias: 'Select', axis: false, gamepad-index: 8, bundle-index: 12});
-          joy1.create({alias: 'Start', axis: false, gamepad-index: 9, bundle-index: 13});
-          joy1.create({alias: 'Left Stick Button', axis: false, gamepad-index: 10, bundle-index: 14});
-          joy1.create({alias: 'Right Stick Button', axis: false, gamepad-index: 11, bundle-index: 15});
-          joy1.create({alias: 'D-Pad Up', axis: false, gamepad-index: 12, bundle-index: 16});
-          joy1.create({alias: 'D-Pad Down', axis: false, gamepad-index: 13, bundle-index: 17});
-          joy1.create({alias: 'D-Pad Left', axis: false, gamepad-index: 14, bundle-index: 18});
-          joy1.create({alias: 'D-Pad Right', axis: false, gamepad-index: 15, bundle-index: 19});
-          joy1.create({alias: 'Aux One', axis: false, gamepad-index: 16, bundle-index: 20});
-          joy1.create({alias: 'Aux Two', axis: false, gamepad-index: 17, bundle-index: 21});
-          joy1.create({alias: 'Aux Three', axis: false, gamepad-index: 18, bundle-index: 22});
-          joy1.create({alias: 'Aux Four', axis: false, gamepad-index: 19, bundle-index: 23});
-
-        */
-
         // update number of connected joysticks
         if (gamepads) {
           this.numJoysticks = gamepads.length;
+
+          if (this.numJoysticks > 0 && !joy1_mapped) {
+            joy1_controller = new ControllerCollection();
+            joy1_mapped = true;
+            joy1_id = gamepads[0].id;
+            initializeController(joy1_controller);
+          }
+          if (this.numJoysticks > 1 && !joy2_mapped) {
+            joy2_controller = new ControllerCollection();
+            joy2_mapped = true;
+            joy2_id = gamepads[1].id;
+            initializeController(joy2_controller);
+          }
+          if (this.numJoysticks > 2 && !joy3_mapped) {
+            joy3_controller = new ControllerCollection();
+            joy3_mapped = true;
+            joy3_id = gamepads[2].id;
+            initializeController(joy3_controller);
+          }
+          if (this.numJoysticks > 3 && !joy4_mapped) {
+            joy4_controller = new ControllerCollection();
+            joy4_mapped = true;
+            joy4_id = gamepads[3].id;
+            initializeController(joy4_controller);
+          }
+
+          // TODO: handle disconnects
+
           for (var i=0;i<this.callbacks.length;i++) { 
             this.callbacks[i].handleJoyCountChange(gamepads.length);
           }
--- js/models/controllerbutton.js
+++ js/models/controllerbutton.js
@@ -6,8 +6,8 @@ define([
     defaults: {
       'alias': 'Left Stick Horizontal',  // the name for this button to show the user
       'axis': true, // is this an axis or button?
-      'gamepad-index': 0, // the axis or button index on the gamepad object for the browser
-      'bundle-index': 0,  // the axis or button index that will be sent over the network as a robotopen bundle
+      'gamepadIndex': 0, // the axis or button index on the gamepad object for the browser
+      'bundleIndex': 0,  // the axis or button index that will be sent over the network as a robotopen bundle
     }
   });
   // Return the model
