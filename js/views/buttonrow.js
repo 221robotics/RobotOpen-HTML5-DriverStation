b73ec0bf0bb3ccb1dc91ef2cfe1189f74ab00728
Eric Barch <ericb@ericbarch.com>
2013-01-22 00:40:47 -0500

    List view that displays all mapped buttons.

---
+++ js/views/buttonrow.js
@@ -0,0 +1,41 @@
+define([
+  'jquery',
+  'underscore',
+  'backbone',
+], function($, _, Backbone){
+
+  var ButtonRowView = Backbone.View.extend({
+    tagName: 'tr', // name of tag to be created        
+
+    // `initialize()` now binds model change/removal to the corresponding handlers below.
+    initialize: function(){
+      _.bindAll(this, 'render', 'unrender', 'remove'); // every function that uses 'this' as the current object should be in here
+
+      this.model.bind('change', this.render);
+      this.model.bind('remove', this.unrender);
+    },
+    // `render()` now includes two extra `span`s corresponding to the actions swap and delete.
+    render: function(){
+      // chrome app restrictions blow so we have to define our template inline
+      var buttonIdentifier;
+      if (this.model.get('axis'))
+        buttonIdentifier = "Axis_"+this.model.get('gamepadIndex');
+      else
+        buttonIdentifier = "Button_"+this.model.get('gamepadIndex');
+
+      $(this.el).html('<td class="primary">' + this.model.get('alias') + '</td><td><a class="btn btn-primary btn-small">' + buttonIdentifier + '</a></td>');
+      return this; // for chainable calls, like .render().el
+    },
+    // `unrender()`: Makes Model remove itself from the DOM.
+    unrender: function(){
+      $(this.el).remove();
+    },
+    // `remove()`: We use the method `destroy()` to remove a model from its collection. Normally this would also delete the record from its persistent storage, but we have overridden that (see above).
+    remove: function(){
+      this.model.destroy();
+    }
+  });
+  
+  // Our module now returns our view
+  return ButtonRowView;
+});
--- js/views/controller.js
+++ js/views/controller.js
@@ -2,22 +2,22 @@ define([
   'jquery',
   'underscore',
   'backbone',
-  'collections/controller'
-], function($, _, Backbone, ControllerCollection){
+  'collections/controller',
+  'views/buttonrow'
+], function($, _, Backbone, ControllerCollection, ButtonRowView){
   var ControllerView = Backbone.View.extend({
     initialize: function(options){
-      _.bindAll(this, 'buttonUpdate'/*, 'render', 'updateBundles', 'appendItem', 'clearAll'*/); // every function that uses 'this' as the current object should be in here
+      _.bindAll(this, 'buttonUpdate', 'render', 'appendItem'); // every function that uses 'this' as the current object should be in here
 
       this.el = $('#'+options.localIdentifier);
       
       this.collection = new ControllerCollection([], {localIdentifier: options.localIdentifier})
-      //this.collection.bind('add', this.appendItem); // collection event binder
-
-      //this.render();
+      this.collection.bind('add', this.appendItem); // collection event binder
+      this.collection.bind('reset', this.render);
     },
     buttonUpdate: function(axis, id) {
 
-    }/*,
+    },
     render: function(){
       var self = this;
 
@@ -25,41 +25,12 @@ define([
         self.appendItem(item);
       }, this);
     },
-    clearAll: function(){
-      // remove all items
-      while (this.collection.models.length > 0) {
-        this.collection.remove(this.collection.models[0]);
-      }
-      // clear the view
-      this.$el.html('');
-    },
-    updateBundles: function(bundles){
-      var self = this;
-
-      // if the DS item already exists, update it!
-      _.each(bundles, function(bundle) {
-        var found = false;
-
-        self.collection.find(function(item) {
-          if (item.get('name') === bundle.get('name')) {
-            item.set({value: bundle.get('value'), type: bundle.get('type'), percent: bundle.get('percent')});
-            found = true;
-            // clean up the intermediary model
-            bundle.destroy();
-          }
-        });
-
-        // didn't find it, add it to the collection
-        if (!found)
-          self.collection.add(bundle);
-      });
-    },
     appendItem: function(item){
-      var itemView = new ItemView({
+      var itemView = new ButtonRowView({
         model: item
       });
-      this.$el.append(itemView.render().el);
-    }*/
+      this.el.append(itemView.render().el);
+    }
   });
 
   // Our module now returns our view
